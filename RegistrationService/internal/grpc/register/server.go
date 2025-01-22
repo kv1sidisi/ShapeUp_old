dd775ce74d093d34c07f815be43d0741b49913b6
kvisidisi <lyapunovvadim5002@gmail.com>
2025-01-22 16:45:08 +0300

    add: go grpc package add: grpc Register handler

---
+++ RegistrationService/internal/grpc/register/server.go
@@ -0,0 +1,21 @@
+package register
+
+import (
+	regv1 "RegistrationService/api/pb"
+	"context"
+	"google.golang.org/grpc"
+)
+
+type serverAPI struct {
+	regv1.UnimplementedRegistrationServer
+}
+
+func Register(gRPC *grpc.Server) {
+	regv1.RegisterRegistrationServer(gRPC, &serverAPI{})
+}
+func (s *serverAPI) Register(
+	ctx context.Context,
+	in *regv1.RegisterRequest,
+) (*regv1.RegisterResponse, error) {
+	panic("implement me")
+}
