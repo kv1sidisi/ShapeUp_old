c634e61c42243ab5005130f4505fda744015bf57
kvisidisi <lyapunovvadim5002@gmail.com>
2025-01-30 13:30:51 +0300

    add: login user server method implementation add: database methods

--- AuthenticationService/internal/service/auth.go
+++ AuthenticationService/internal/service/auth.go
@@ -1,35 +1,72 @@
 package service
 
 import (
+	"AuthenticationService/internal/config"
+	"AuthenticationService/internal/service/helpers"
 	"context"
+	"github.com/asaskevich/govalidator"
 	"log/slog"
 )
 
 // AuthService struct represents the sending service and it is implementation of bottom layer of sending method of application.
 type AuthService struct {
 	log     *slog.Logger
+	cfg     *config.Config
 	storage AuthManager
 }
 
 type AuthManager interface {
-	FindUser(ctx context.Context,
+	FindUserByName(ctx context.Context,
 		username string,
 		password string,
 	) (uid int64, err error)
+	FindUserByEmail(ctx context.Context,
+		username string,
+		password string,
+	) (uid int64, err error)
+	AddSession(ctx context.Context,
+		uid int64,
+		accessToken string,
+		refreshToken string,
+	) (err error)
 }
 
 // New returns a new instance of AuthService service.
-func New(log *slog.Logger, storage AuthManager) *AuthService {
-	return &AuthService{log: log, storage: storage}
+func New(log *slog.Logger, cfg *config.Config, storage AuthManager) *AuthService {
+	return &AuthService{log: log,
+		storage: storage,
+		cfg:     cfg}
 }
 
 func (as *AuthService) LoginUser(
 	ctx context.Context,
 	username string,
 	password string,
-) (userId int64, err error) {
-	//TODO: try to find user in db
-	//TODO: generate jwt and refresh tokens
-	//TODO: return jwt and refresh tokens
-	return 1, nil
+) (userId int64, accessToken string, refreshToken string, err error) {
+	// TODO: try pattern chain of responsibility
+	if govalidator.IsEmail(username) {
+		// if username is email
+		userId, err = as.storage.FindUserByEmail(ctx, username, password)
+	} else {
+		// if username is login
+		userId, err = as.storage.FindUserByName(ctx, username, password)
+	}
+
+	if err != nil {
+		return 0, "", "", err
+	}
+
+	accessToken, err = jwt.GenerateAccessToken(userId, as.cfg.JWTSecret)
+	if err != nil {
+		return 0, "", "", err
+	}
+	refreshToken, err = jwt.GenerateRefreshToken(userId, as.cfg.JWTSecret)
+	if err != nil {
+		return 0, "", "", err
+	}
+
+	if err := as.storage.AddSession(ctx, userId, accessToken, refreshToken); err != nil {
+		return 0, "", "", err
+	}
+	return userId, accessToken, refreshToken, nil
 }
