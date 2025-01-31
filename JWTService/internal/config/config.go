f138505c30899792508cb10632c6104bbc08dbec
kvisidisi <lyapunovvadim5002@gmail.com>
2025-01-31 16:58:41 +0300

    add: config parser config.go

---
+++ JWTService/internal/config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	GRPC GRPCConfig `yaml:"grpc"`
+	JWT  JWTConfig  `yaml:"jwt"`
+}
+
+type JWTConfig struct {
+	AccessTokenSecretKey  string `yaml:"access_token_secret_key"`
+	RefreshTokenSecretKey string `yaml:"refresh_token_secret_key"`
+}
+
+type GRPCConfig struct {
+	Port    string        `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+// MustLoad gets config path and panics if there is any errors in parsing config.
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadByPath(path)
+}
+
+// MustLoadByPath gets config path from arguments and panics if there is any errors in parsing config.
+func MustLoadByPath(path string) *Config {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file not found: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic(err)
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string "".
+func fetchConfigPath() string {
+	var res string
+
+	// --config="path/to/config.yaml"
+	flag.StringVar(&res, "config", "", "config file path")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
+}
