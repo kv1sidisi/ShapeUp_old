1831948597bd7c65569ebe8596381fc2b32b5cdb
kvisidisi <lyapunovvadim5002@gmail.com>
2025-02-18 22:09:44 +0300

    feat: server method to update base user attributes using service

--- services/UsrDataSvc/internal/grpc/grpcsrv/grpcsrv.go
+++ services/UsrDataSvc/internal/grpc/grpcsrv/grpcsrv.go
@@ -2,6 +2,7 @@ package grpcsrv
 
 import (
 	"context"
+	"github.com/kv1sidisi/shapeup/pkg/errdefs"
 	"github.com/kv1sidisi/shapeup/services/usrdatasvc/api/grpc/pb/usrdatasvc"
 	"github.com/kv1sidisi/shapeup/services/usrdatasvc/internal/config"
 	"google.golang.org/grpc"
@@ -11,7 +12,7 @@ import (
 
 // UsrDataSvc service for serverAPI.
 type UsrDataSvc interface {
-	UpdUsr(ctx context.Context, bsusrattr *usrdatasvc.BsUsrAttr, mask *fieldmaskpb.FieldMask) error
+	UpdUsr(ctx context.Context, bsusrattr *usrdatasvc.BsUsrAttr, mask *fieldmaskpb.FieldMask) (updbsusrattr *usrdatasvc.BsUsrAttr, err error)
 }
 
 // serverAPI handler for the gRPC server.
@@ -37,4 +38,41 @@ func RegisterServer(gRPC *grpc.Server,
 			cfg:     cfg,
 			log:     log,
 		})
+}
+
+// UpdBsUsrAttr is the GRPC server handler method. Updates base user's attributes.
+//
+// Returns:
+//
+//   - A pointer to UpdBsUsrAttrRequest if successful.
+//
+//   - An error if: Request is invalid. Error while updating base user's attributes through service.
+func (s *serverAPI) UpdBsUsrAttr(ctx context.Context, req *usrdatasvc.UpdBsUsrAttrRequest) (*usrdatasvc.UpdBsUsrAttrResponse, error) {
+	const op = "grpcsrv.UpdBsUsrAttr"
+	log := s.log.With(slog.String("op", op))
+
+	if req.GetUser() == nil {
+		log.Error("invalid user in grpc request")
+		return nil, errdefs.InvalidRequest
+	}
+
+	if ctx.Value("uid") == nil {
+		log.Error("invalid uid in grpc request(context)")
+		return nil, errdefs.InvalidCredentials
+	}
+
+	log.Info("request valid")
+
+	updbsusrattr, err := s.usrData.UpdUsr(ctx, req.GetUser(), req.GetUpdMask())
+
+	if err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	log.Info("user attributes updated successfully")
+
+	return &usrdatasvc.UpdBsUsrAttrResponse{
+		User: updbsusrattr,
+	}, nil
 }
