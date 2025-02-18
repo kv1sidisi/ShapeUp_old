93454fbb99328f1f961bd7ef41976ba03986e1ca
kvisidisi <lyapunovvadim5002@gmail.com>
2025-02-18 20:03:05 +0300

    feat: postgresql connection setup

--- services/UsrDataSvc/cmd/main.go
+++ services/UsrDataSvc/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"github.com/jackc/pgx/v4/pgxpool"
 	loadconfig "github.com/kv1sidisi/shapeup/pkg/config"
+	"github.com/kv1sidisi/shapeup/pkg/database/pgcl"
 	"github.com/kv1sidisi/shapeup/pkg/logger"
 	"github.com/kv1sidisi/shapeup/services/usrdatasvc/internal/config"
+	"log/slog"
 )
 
 func main() {
@@ -12,4 +16,30 @@ func main() {
 
 	log := logger.SetupLogger(cfg.Env)
 
+	log.Info("starting up", slog.String("env", cfg.Env))
+
+	postgresqlClient := mustConnectToDatabase(cfg)
+	log.Info("connected to database")
+
+	application
+
+}
+
+// mustConnectToDatabase panics if setupDatabaseConnection fails
+func mustConnectToDatabase(cfg *config.Config) *pgxpool.Pool {
+	postgresqlClient, err := setupDatabaseConnection(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return postgresqlClient
+}
+
+// setupDatabaseConnection connect to database
+// returns pgx client.
+func setupDatabaseConnection(cfg *config.Config) (*pgxpool.Pool, error) {
+	postgresqlClient, err := pgcl.NewClient(context.TODO(), 3, &cfg.Storage)
+	if err != nil {
+		return nil, err
+	}
+	return postgresqlClient, nil
 }
